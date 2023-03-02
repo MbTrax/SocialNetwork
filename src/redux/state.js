c500b34e06302cbaf44a2a23bd171f131f9c1f4f
MbTrax <danilada20100@gmail.com>
2023-03-02 13:45:48 +0300

    V1
    Первый билд

--- src/redux/state.js
+++ src/redux/state.js
@@ -7,7 +7,7 @@ const store = {
       newPostText: '',
       postsData: [
         { id: '1', message: 'Make in your DREAMS!', likeCount: 34, image: 'https://coolsen.ru/wp-content/uploads/2021/09/image143-6.jpg' },
-        { id: '2', message: 'Do it!', likeCount: 10, image: 'https://kartinkin.net/pics/uploads/posts/2022-08/1660109268_44-kartinkin-net-p-anime-v-stim-s-chernim-fonom-krasivo-45.jpg' },
+        { id: '2', message: 'Do it!', likeCount: 10, image: 'https://i.pinimg.com/originals/18/c7/2c/18c72c635bcd4bddad6c6f27ab365aad.jpg' },
       ]
     },
     dialogsPage: {
